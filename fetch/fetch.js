a04b7d8661eae9f85ed9adda0c627014947ef928
Surohak <Surohak@users.noreply.github.com>
2018-12-12 12:59:07 +0400

    initial commit

---
+++ fetch/fetch.js
@@ -0,0 +1,25 @@
+fetch('https://ghibliapi.herokuapp.com/locations', {
+    method: 'get'
+})
+    .then(function (response) {
+        return response.json()
+            .then(
+                function (data) {
+                    let k = '<tbody';
+                     for(i = 0;i < data.length; i++){
+                        k += '<tr>';
+                        k += '<td>' + data[i].id + '</td>';
+                        k += '<td>' + data[i].name + '</td>';
+                        k += '<td>' + data[i].climate + '</td>';
+                        k += '<td>' + data[i].terrain + '</td>';
+                        k += '<td>' + data[i].surface_water + '</td>';
+                        k += '</tr>';
+                     }
+                    k+='</tbody>';
+                    document.getElementById('tbody').innerHTML = k;
+                }
+            )
+    }).catch(function (err) {
+    // Error :(
+    console.log('oops!');
+});
